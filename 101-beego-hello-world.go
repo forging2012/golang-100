bc0028c81ac0b3cb5cd0b2493d9374c5cf1e5af9
forging2012 <forging2012@126.com>
2017-06-08 00:39:47 +0800

    Add 101-beego hello world

---
+++ 101-beego-hello-world.go
@@ -0,0 +1,17 @@
+// FILENAME: 101-beego-hello-world.go
+// DATE: 2017/6/8
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 101-beego-hello-world.go
+
+package main
+
+import (
+	//"fmt"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//fmt.Println("This is beego demo.")
+	beego.Run()
+}
