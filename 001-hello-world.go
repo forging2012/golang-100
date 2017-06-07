4e065f9a407eeff34dd54f7ac3f4defc63c244bb
forging2012 <forging2012@126.com>
2017-06-07 23:48:08 +0800

    001-hello world

--- 001-hello-world.go
+++ 001-hello-world.go
@@ -1,1 +1,19 @@
-package golang_100
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+
+//GOROOT=C:/Go
+//GOPATH=C:/Go/bin
+//C:/Go\bin\go.exe build -i -o C:\Users\admin\AppData\Local\Temp\Build_001_hello_world_go_and_rungo -gcflags "-N -l" C:/Users/admin/Documents/wwwgit/golang-100/001-hello-world.go
+//"C:\Program Files\JetBrains\Gogland 171.4424.55\bin\runnerw.exe" "C:\Program Files\JetBrains\Gogland 171.4424.55\plugins\intellij-go-plugin\lib\dlv\windows\dlv.exe" --listen=localhost:56296 --headless=true --api-version=2 --backend=default exec C:\Users\admin\AppData\Local\Temp\Build_001_hello_world_go_and_rungo --
+//API server listening at: 127.0.0.1:56296
+//Hello world!
+//
+//Process finished with exit code 0
