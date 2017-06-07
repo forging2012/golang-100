11dd576da1b1ba3aee54bd0a45c1b3ad12a69d7b
forging2012 <forging2012@126.com>
2017-06-08 00:46:54 +0800

    102-beego-hello-world-with-route

---
+++ 102-beego-hello-world-with-route.go
@@ -0,0 +1,23 @@
+// FILENAME: 102-beego-hello-world-with-route.go
+// DATE: 2017/6/8
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 102-beego-hello-world-with-route.go
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
+func main() {
+	beego.Router("/", &MainController{})
+	beego.Run()
+}
