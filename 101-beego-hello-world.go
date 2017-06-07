a7e25be4d1c7468e1c12c8b6803fd42df9520cdf
forging2012 <forging2012@126.com>
2017-06-08 00:47:22 +0800

    update 101-beego hello world

--- 101-beego-hello-world.go
+++ 101-beego-hello-world.go
@@ -2,16 +2,21 @@
 // DATE: 2017/6/8
 // AUTHOR: FORGING2012@GMAIL.COM
 // Github: https://github.com/forging2012
-// Description: 101-beego-hello-world.go
-
-package main
+// Description: 101-beego-hello-world.gopackage main
 
 import (
-	//"fmt"
 	"github.com/astaxie/beego"
 )
 
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
 func main() {
-	//fmt.Println("This is beego demo.")
+	beego.Router("/", &MainController{})
 	beego.Run()
 }
