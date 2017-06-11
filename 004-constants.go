57d050b3095937fef57dff3c7829336b00b5f771
forging2012 <forging2012@126.com>
2017-06-11 23:33:51 +0800

    004-constants

---
+++ 004-constants.go
@@ -0,0 +1,25 @@
+// FILENAME: 004-constants.go
+// DATE: 2017/6/11
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 004-constants.go
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+	const d = 3e20 / n
+	fmt.Println(d)
+	fmt.Println(int64(d))
+	
+	fmt.Println(math.Sin(n))
+}
