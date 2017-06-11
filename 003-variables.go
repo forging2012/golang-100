6245b1836b49a0e15cf5e77a2a81671d790ca65a
forging2012 <forging2012@126.com>
2017-06-11 23:33:40 +0800

    003-variables

---
+++ 003-variables.go
@@ -0,0 +1,29 @@
+// FILENAME: 003-variables.go
+// DATE: 2017/6/11
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 003-variables.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a string = "This is a test text."
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+	
+	f := "short"
+	fmt.Println(f)
+	
+}
