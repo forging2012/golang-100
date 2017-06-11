2146157f466cab18e0d45a253eb58a916b6328e4
forging2012 <forging2012@126.com>
2017-06-12 00:37:35 +0800

    005-arrays

---
+++ 008-arrays.go
@@ -0,0 +1,32 @@
+// FILENAME: 008-arrays.go
+// DATE: 2017/6/12
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 008-arrays.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("a:",a)
+	fmt.Println("set:",a)
+	fmt.Println("get:", a[4])
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var towD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			towD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", towD)
+}
