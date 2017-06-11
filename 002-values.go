4c1ff89d151d5e1b9dda82a7053e1f1c5cf2810d
forging2012 <forging2012@126.com>
2017-06-11 23:33:25 +0800

    002-values

---
+++ 002-values.go
@@ -0,0 +1,54 @@
+// FILENAME: 002-values.go
+// DATE: 2017/6/11
+// AUTHOR: FORGING2012@GMAIL.COM
+// Github: https://github.com/forging2012
+// Description: 002-values.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+2" + "=",1+2 )
+	fmt.Println("7/2" + "=",7/2 )
+	fmt.Println("7.0/2" + "=",7.0/2 )
+	fmt.Println("7.0/2.0" + "=",7.0/2.0 )
+	fmt.Println("7/2.0" + "=",7/2.0 )
+	fmt.Println("7.0/3.5" + "=",7.0/3.5 )
+	
+	fmt.Println( true && true )
+	fmt.Println( true || true )
+	fmt.Println( true && false )
+	fmt.Println( true || false )
+	fmt.Println( false || false )
+	fmt.Println( false && false )
+	fmt.Println( ! true )
+	fmt.Println( ! false )
+
+}
+
+
+/*
+"C:\Program Files\JetBrains\Gogland 171.4424.55\bin\runnerw.exe" C:/Go\bin\go.exe run C:/Users/admin/Documents/wwwgit/golang-100/002-values.go
+golang
+1+2= 3
+7/2= 3
+7.0/2= 3.5
+7.0/2.0= 3.5
+7/2.0= 3.5
+7.0/3.5= 2
+true
+true
+false
+true
+false
+false
+false
+true
+
+Process finished with exit code 0
+
+*/
