d39d0534bf85d6da37f6c9485a34e2f64437f53f
forging2012 <forging2012@126.com>
2017-06-20 17:27:10 +0800

    101-beego-hello-world

--- 101-beego-hello-world.go
+++ 101-beego-hello-world.go
@@ -3,6 +3,7 @@
 // AUTHOR: FORGING2012@GMAIL.COM
 // Github: https://github.com/forging2012
 // Description: 101-beego-hello-world.gopackage main
+package main
 
 import (
 	"github.com/astaxie/beego"
